fb96a133be8ab8234a66124052465cf605f85bd0
Amoo Abeeb <amooabeeb0@gmail.com>
2019-02-15 19:28:55 +0100

    Chang menu bar height

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,22 +1,20 @@
 @extends('layouts.frontend')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-warning">
-                <div class="panel panel-warning">Register</div>
-                  <p>@if($message =Session::get('success'))
-    <div class="alert alert-success">
-  <p>
-    {{$message}}
-  </p>
-</div>
-@endif</p>
 
+<div class="registration-container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2 col-sm-12 registration-panel">
+            <div class="panel panel-warning">
+                <div class="panel panel-heading panel-heading__auth text-center">REGISTRATION</div>
+                    @if($message = Session::get('success'))
+                        <div class="alert alert-success">
+                            <p>{{$message}}</p>
+                        </div>
+                    @endif
                 <div class="panel-body">
                     {{ Form::model(['route'=>['register'],'method'=>'POST', 'class' => 'form-horizontal']) }}
-                    @include ('auth.form-master');
+                         @include ('auth.form-master')
                     {{ Form::close() }}
                 </div>
             </div>
